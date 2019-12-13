019633b142a6feb8993191d2b2a628d1f864e9f2
Rudolf Unruh <unruh@blausoft.net>
2019-12-13 08:14:52 +0100

    Add more tests

--- test/youtube_parser_test.dart
+++ test/youtube_parser_test.dart
@@ -6,7 +6,7 @@ const _validId = 'v4lid-YT-id';
 
 final _validLinks = <String>[
   'https://www.youtube.com/watch?v=$_validId',
-  'https://www.youtube.com/watch?v=$_validId?t=27m53s',
+  'https://www.youtube.com/watch?v=$_validId&t=27m53s',
   'https://www.youtube.com/watch?v=$_validId&list=xS9cRDfFtF9FM#t=0',
   'https://m.youtube.com/watch?v=$_validId',
   'https://m.youtube.com/watch?v=$_validId&list=xS9cRDfFtF9FM#t=0',
@@ -15,39 +15,66 @@ final _validLinks = <String>[
 ];
 
 void main() {
+  // Add some variations
+  final additionalValidLinks = <String>[];
+  for (final link in _validLinks) {
+    additionalValidLinks.add(link.replaceAll('https://', 'http://'));
+  }
+  _validLinks.addAll(additionalValidLinks);
+
   for (final link in _validLinks) {
     test('Should return ID: $link', () {
       expect(getIdFromUrl(link), equals(_validId));
     });
   }
 
+
   test('Throws error on null', () {
     expect(() => getIdFromUrl(null), throwsA(const TypeMatcher<ArgumentError>()));
   });
 
-  test('Should return null for bad url', () {
-    final invalidLinks = <String>[
+
+  final invalidLinks = <String>[
+      '', ' ', '\n',
       'foobar',
       'https://www.any-website.com',
       'https://www.notyoutube.com/watch?v=$_validId',
-      'https://www.youtube.com/watch?v=CBWlfU_na-',
+      'https://www.notyoutube.com/$_validId',
+      'https://youtu.be',
+      'https://youtube.com',
+      'https://m.youtube.com',
+      'https://www.youtube.com',
+      'https://www.youtube.com/watch',
       'https://n.youtube.com/watch?v=$_validId',
       'https://m.youtube.com/wtch?v=$_validId&list=xS9cRDfFtF9FM#t=0',
-      'https://youtu.be/CBWlfU_na-',
-      'https://youtu.de/$_validId',
-      'https://www.youtu.be/$_validId'
+      'https://www.youtu.be/$_validId '
     ];
 
-    // Test for misplaced whitespaces
-    for (final link in _validLinks) {
-      invalidLinks
-          ..add(' $link ')
-          ..add(' $link')
-          ..add('$link ');
-    }
+  // Add some more invalid links to test
+  for (final link in _validLinks) {
+    invalidLinks
 
-    for (final link in invalidLinks) {
+      // Add whitespaces
+      ..add(' $link ')
+      ..add(' $link')
+      ..add('$link ')
+
+      // Add invalid ids
+      ..add(link.replaceAll(_validId, 't00-_short'))
+      ..add(link.replaceAll(_validId, 't000-_Looong'))
+      ..add(link.replaceAll(_validId, 'w1tha sp4c3'))
+      ..add(link.replaceAll(_validId, 'inva!id.chr'))
+
+      // Add invalid schemes
+      ..add(link.replaceAll(RegExp(r'http[s]*:\/\/'), ''))
+      ..add(link.replaceAll(RegExp(r'http[s]*:\/\/'), 'htpts://'))
+      ..add(link.replaceAll(RegExp(r'http[s]*:\/\/'), 'https:/'));
+  }
+
+  // Run tests
+  for (final link in invalidLinks) {
+    test('Should return null: $link', () {
       expect(getIdFromUrl(link), isNull);
-    }
-  });
+    });
+  }
 }
